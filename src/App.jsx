1308af4deb9aec6d96fbdcf872443200b3696d13
Alina <alinavalentinaiacob@gmail.com>
2026-01-02 15:49:42 +0200

    select

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import './App.css'
+import Select from "react-select"
 import Header from "./components/header/Header.jsx";
 import Carousel from "./components/carousel/Carousel.jsx";
 
 function App() {
+   const options=[
+       {
+      "value": "NXT","label":"NextPower Energy"
+       },
+       {
+           "value": "NEE","label":"NextEra Energy"
+       },
+       {
+           "value": "LEU","label":"Centrus Energy"
+       },
+       {
+           "value": "UUUU","label":"Energy Fuels"
+       },
+       {
+           "value": "EPHN","label":"Enphase Energy"
+       },
+       {
+       "value": "SPX","label":"S&P 500"
+       }
 
+   ]
 
   return (
     <>
